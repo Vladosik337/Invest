acbd7213a8a68dbc1ea7a30ce5b19b4d1f08c665
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-09-16 02:07:07 +0300

    import git

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import '../scss/style.scss'
+
+
+// dark theme
+// const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+// if(isDarkMode){
+//     document.querySelector("body").style.backgroundColor = '#2C3136';
+//     document.querySelector("body").style.color = '#fff';
+//
+// }
