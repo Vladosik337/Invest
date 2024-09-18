6c3c3f068721985297d7379c5986c8bcfde44eba
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-09-18 22:16:48 +0300

    [FEAT] add header section

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,21 @@ import '../scss/style.scss'
 //     document.querySelector("body").style.color = '#fff';
 //
 // }
+
+
+// -----------------Burger-----------------
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    document.body.classList.toggle("no-scroll");
+});
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+    document.body.classList.remove("no-scroll");
+}));
